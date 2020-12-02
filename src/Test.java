e44a4326f0546d8a5e675228939dbc0e3aab88b4
Pavel Setsko <pavelsetsko@gmail.com>
2020-12-02 23:28:01 +0300

    1

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int[] mass = {1, 2, 3, 4, 5, 6};
+		
+		for (int i = 0; i < mass.length; i++) {
+			System.out.println(mass[i]);
+		}
+
+	}
+
+}
